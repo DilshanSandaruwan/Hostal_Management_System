eedcaef87751d65eda7bb8c39673153d45534b20
DilshanSandaruwan <sadaruwandilshan627@gmail.com>
2023-09-08 13:02:59 +0530

    Create Rooms interface

--- src/lk/ijse/bo/custom/RoomBO.java
+++ src/lk/ijse/bo/custom/RoomBO.java
@@ -5,13 +5,5 @@ import lk.ijse.bo.SuperBO;
 import java.util.ArrayList;
 
 public interface RoomBO extends SuperBO {
-    Boolean addRoom(RoomsDTO roomsDTO);
 
-    Boolean deleteRoom(RoomsDTO roomsDTO);
-
-    ArrayList<RoomsDTO> getRoomsData();
-
-    //String getCurrentID();
-
-    Boolean updateRoom(RoomsDTO roomsDTO);
 }
