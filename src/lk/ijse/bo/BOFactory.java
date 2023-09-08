7f339d23d3ee83071f20c2270b4fe1ec80e183eb
DilshanSandaruwan <sadaruwandilshan627@gmail.com>
2023-09-08 13:21:37 +0530

    commit updated

--- src/lk/ijse/bo/BOFactory.java
+++ src/lk/ijse/bo/BOFactory.java
@@ -1,5 +1,34 @@
 package lk.ijse.bo;
 
 public class BOFactory {
+    private static BOFactory boFactory;
 
+    private BOFactory() {
+    }
+
+    public static BOFactory getBoFactory() {
+        return boFactory == null ? boFactory = new BOFactory() : boFactory;
+    }
+
+    public enum Type {
+        STUDENT,
+        ROOM,
+        RECEPTION,
+        USER
+    }
+
+    public SuperBO getBO(Type type) {
+        switch (type) {
+            case STUDENT:
+                return new StudentBOImpl();
+            case ROOM:
+                return new RoomsBOImpl();
+            case RECEPTION:
+                return new ReservationBOImpl();
+            case USER:
+                return new UserBOImpl();
+            default:
+                return null;
+        }
+    }
 }
